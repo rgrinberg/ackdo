bdda81f57a0b5c9a9edf70f776f540a3a746880b
Rudi Grinberg <rudi.grinberg@gmail.com>
2012-11-11 17:46:07 -0500

    style

--- ackdo.ml
+++ ackdo.ml
@@ -49,11 +49,12 @@ end
 module String = struct
   include String
   let split s ~by = 
-    if by = "" then (s, "") else
+    if by = "" then (s, "") else begin
       let re = Str.regexp ("\(^.*\)" ^ (Str.quote by) ^ "\(.*\)$") in
       let smatch = Str.string_match re s 0 in
       if not smatch then raise Not_found
       else Str.(matched_group 1 s, matched_group 1 s)
+    end
 end                             
 
 module LCS = struct 
